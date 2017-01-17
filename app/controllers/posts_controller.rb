8bc240888b3107fa5c271fab7531bda2b9d4a2f8
Wendersonandes <oldmaverick@gmail.com>
2017-01-17 19:18:07 -0200

    Allowed users to add tags to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,6 @@ class PostsController < ApplicationController
 	end
 
 	def post_params
-		params.require(:post).permit(:title, :body)
+		params.require(:post).permit(:title, :body, :all_tags)
 	end
 end
